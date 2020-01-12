72b5ea09f7989130ed6dd519747989400bb63cc5
Tou Her <toumoher@gmail.com>
2020-01-12 07:41:12 -0700

    Trivia Game

---
+++ assets/javascript/app.js
@@ -0,0 +1,100 @@
+// Questions and Answers
+
+//var fishSpecies = ["Black Crappie", "Blue Catfish", "Bluegill", "Brook Trout", "Brown Trout", "Carp", "Channel Catfish", "Flathead Catfish", "Freshwater Drum", "Green Sunfish", "Largemouth Bass", "Muskellunge", "Rainbow Trout", "Smallmouth Bass", "Stripped Bass", "Walleye", "White Crappie"]
+
+//var images = ["BlackCrappie.png", "BlueCatfish.png", "Bluegill.png", "BrookTrout.png", "BrookTrout.png", "BrownTrout.png", "Carp.png", "ChannelCatfish.png", "FlatheadCatfish.png", "FreshwaterDrum.png", "GreenSunFish.png", "largemouthbass.png", "Muskellunge.png", "RainbowTrout.png", "SmallmouthBass.png", "StripedBass.png", "Walleye.png", "WhiteCrappie.png"]
+
+$(document).ready(function () { 
+
+var questions = [
+    {
+        question: "<img src=\"assets\images\BlackCrappie.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Blue Catfish", "Bluegill", "Brook Trout", "Brown Trout", "Carp"],
+        answer: 0,
+    },
+    {
+        question: "<img src=\"assets\images\BlueCatfish.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Blue Catfish", "Bluegill", "Brook Trout", "Brown Trout", "Carp"],
+        answer: 1,
+    },
+    {
+        question: "<img src=\"assets\images\Bluegill.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Blue Catfish", "Bluegill", "Brook Trout", "Brown Trout", "Carp"],
+        answer: 2,
+    },
+    {
+        question: "<img src=\"assets\images\BrookTrout.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Blue Catfish", "Bluegill", "Brook Trout", "Brown Trout", "Carp"],
+        answer: 3,
+    },
+    {
+        question: "<img src=\"assets\images\BrownTrout.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Blue Catfish", "Bluegill", "Brook Trout", "Brown Trout", "Carp"],
+        answer: 4,
+    },
+    {
+        question: "<img src=\"assets\images\Carp.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Blue Catfish", "Bluegill", "Brook Trout", "Brown Trout", "Carp"],
+        answer: 5,
+    },
+];
+
+    // Optional Questions
+    /*
+    {
+        question: "<img src=\"assets\images\ChannelCatfish.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 6,
+    },
+    {
+        question: "<img src=\"assets\images\FlatheadCatfish.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 7,
+    },
+    {
+        question: "<img src=\"assets\images\FreshwaterDrum.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 9,
+    },
+    {
+        question: "<img src=\"assets\images\GreenSunfish.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 10,
+    },
+    {
+        question: "<img src=\"assets\images\largemouthbass.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 11,
+    },
+    {
+        question: "<img src=\"assets\images\Muskellunge.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 12,
+    },
+    {
+        question: "<img src=\"assets\images\RainbowTrout.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 13,
+    },
+    {
+        question: "<img src=\"assets\images\Smallmouthbass.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 14,
+    },
+    {
+        question: "<img src=\"assets\images\StripedBass.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 15,
+    },
+    {
+        question: "<img src=\"assets\images\Walleye.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 16,
+    },
+    {
+        question: "<img src=\"assets\images\WhiteCrappie.png\"/><br>What species of fish is this?",
+        option: ["Black Crappie", "Rainbow Trout", "Carp", "Bluegill", "Striped Bass"],
+        answer: 8,
+    },
+    */
+});
